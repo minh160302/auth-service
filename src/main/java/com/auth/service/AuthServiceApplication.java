57f2e5a64489845f2937bc4ef96496c4fcb36b6b
minh160302 <minh160302@gmail.com>
2021-07-06 22:05:18 +0700

    disable eureka --temporary

--- src/main/java/com/auth/service/AuthServiceApplication.java
+++ src/main/java/com/auth/service/AuthServiceApplication.java
@@ -3,16 +3,16 @@ package com.auth.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableFeignClients
-@EnableEurekaClient
+//@EnableFeignClients
+//@EnableEurekaClient
 @SpringBootApplication
 public class AuthServiceApplication {
 
